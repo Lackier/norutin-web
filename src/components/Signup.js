142a64a4da231ddf2ae27b005aa8c342fa430613
Daniel <chernov.daniel0@gmail.com>
2021-04-14 00:31:01 +0300

    front-1: added connection with backend during signUp

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,15 +2,17 @@ import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
+import $ from 'jquery';
 
 export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
-    const { signup } = useAuth()
+    const { login } = useAuth()
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
     const history = useHistory()
+    let email, password
 
     async function handleSubmit(e) {
         e.preventDefault()
@@ -19,16 +21,43 @@ export default function Signup() {
             return setError("Passwords do not match")
         }
 
+        email = emailRef.current.value
+        password = passwordRef.current.value
+
         try {
+            const url = "http://127.0.0.1:8080/api/users/signup"
+            $.ajax({
+                url: url,
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                },
+                type: "Post",
+                data: {
+                    "email": emailRef.current.value,
+                    "password": passwordRef.current.value,
+                    "phoneNumber": "+77777777778",
+                    "name": "aaaaaab_user"
+                },
+                success: function (result) {
+                    _login()
+                },
+                error: function (error) {
+                    console.log('Error ' + error)
+                }
+            })
+
             setError("")
             setLoading(true)
-            await signup(emailRef.current.value, passwordRef.current.value)
             history.push("/")
         } catch {
             setError("Failed to create an account")
         }
 
         setLoading(false)
+    }
+
+    async function _login() {
+        await login(email, password)
     }
 
     return (
